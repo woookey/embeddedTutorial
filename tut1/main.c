2e12b0926621018e2b1bd23d0a1ec6870cbf0456
woookey <woo.barczyk@gmail.com>
2019-03-02 18:48:10 +0000

    add switching green LED on to main

--- tut1/main.c
+++ tut1/main.c
@@ -5,11 +5,15 @@ static void switchLEDOn(void);
 int main(void)
 {
 	switchLEDOn();
-	while(1);
 	return 0;
 }
 
 static void switchLEDOn(void)
 {
+	// Enable clock for GPIOD
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
 
+	// Set GPIOD Pin 12 to Output and switch it on
+	GPIOD->MODER |= GPIO_MODER_MODE12_0;
+	GPIOD->ODR |= GPIO_ODR_OD12;
 }
