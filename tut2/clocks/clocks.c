49e5696a62a1a66b9c0ba88053f71221e49f1131
woookey <woo.barczyk@gmail.com>
2021-05-24 14:05:26 +0200

    update makefiles and get rid of gpio dir for tut2

--- tut2/clocks/clocks.c
+++ tut2/clocks/clocks.c
@@ -2,31 +2,31 @@
 #include "stm32f4xx_hal_rcc.h"
 
 static RCC_ClkInitTypeDef rccClkInstance =
-        {
-                .ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2,
-                .SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
-                .AHBCLKDivider = RCC_SYSCLK_DIV1,
-                .APB1CLKDivider = RCC_HCLK_DIV1,
-                .APB2CLKDivider = RCC_HCLK_DIV1,
-        };
+{
+    .ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2,
+    .SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
+    .AHBCLKDivider = RCC_SYSCLK_DIV1,
+    .APB1CLKDivider = RCC_HCLK_DIV1,
+    .APB2CLKDivider = RCC_HCLK_DIV1,
+};
 
 static RCC_OscInitTypeDef clock_setup = {
-        .OscillatorType = RCC_OSCILLATORTYPE_HSE,
-        .HSEState = RCC_CR_HSEON,
-        .PLL = {
-                .PLLState = RCC_PLL_ON,
-                .PLLSource = RCC_PLLSOURCE_HSE,
-                .PLLM = 8,
-                .PLLN = 336,
-                .PLLP = RCC_PLLP_DIV2,
-        }
+    .OscillatorType = RCC_OSCILLATORTYPE_HSE,
+    .HSEState = RCC_CR_HSEON,
+    .PLL = {
+        .PLLState = RCC_PLL_ON,
+        .PLLSource = RCC_PLLSOURCE_HSE,
+        .PLLM = 8,
+        .PLLN = 336,
+        .PLLP = RCC_PLLP_DIV2,
+    }
 };
 
 void clocks_initialise(void) {
     HAL_RCC_OscConfig(&clock_setup);
     HAL_RCC_ClockConfig(&rccClkInstance, 5);
 
-    // enable Port D
+    /// enable Port D
     __HAL_RCC_GPIOD_CLK_ENABLE();
 }
 
--- tut2/main.c
+++ tut2/main.c
@@ -1,20 +1,22 @@
-#include "stm32f4xx_hal.h"
-
 #include "clocks/clocks.h"
-#include "gpio/gpio.h"
+#include "stm32f4xx_hal.h"
 
 #include <stdbool.h>
 
 int main(void)
 {
-    /**
-     * Enable PLL, and clock for an LED
-     */
+    /// Enable PLL, and clock for an LED
     clocks_initialise();
 
-    gpio_init(gpio_led);
+    GPIO_InitTypeDef led_gpio = {
+        .Pin = GPIO_PIN_12,
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_PULLDOWN,
+        .Speed = GPIO_SPEED_FREQ_HIGH,
+    };
+    HAL_GPIO_Init(GPIOD, &led_gpio);
 	while(1) {
-        gpio_toggle(gpio_led);
+        HAL_GPIO_TogglePin(GPIOD, led_gpio.Pin);
         HAL_Delay(500);
 	}
 }
