b8afbb7627eb9704fc556da3da3e535f58da0796
woookey <lukaszbarczyk@actuatedrobots.com>
2022-07-14 10:52:30 +0200

    add cmd for led blue turn off/on

--- tut4_uart_comms/main.c
+++ tut4_uart_comms/main.c
@@ -1,11 +1,14 @@
 #include "clocks/clocks.h"
-#include "comms/comms.h"
+#include "comms_driver/comms_driver.h"
 #include "stm32f4xx_hal.h"
 
 #include <stdbool.h>
 #include <stddef.h>
 #include <string.h>
 
+/// variables and definitions for running tasks
+#define TASKS_FREQUENCY_IN_MS (uint16_t)100
+#define TASKS_FREQUENCY_THRESHOLD (TASKS_FREQUENCY_IN_MS-1)
 volatile uint16_t time_counter = 0;
 volatile bool background_processed = false;
 
@@ -14,6 +17,13 @@ static GPIO_InitTypeDef led_gpio = {
     .Mode = GPIO_MODE_OUTPUT_PP,
     .Pull = GPIO_PULLDOWN,
     .Speed = GPIO_SPEED_FREQ_HIGH
+};
+
+static GPIO_InitTypeDef led_blue_gpio = {
+        .Pin = GPIO_PIN_15,
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_PULLDOWN,
+        .Speed = GPIO_SPEED_FREQ_HIGH
 };
 
 static GPIO_InitTypeDef measure_gpio = {
@@ -39,33 +49,35 @@ static GPIO_InitTypeDef uart1_rx_gpio = {
     .Alternate = GPIO_AF7_USART1
 };
 
-static char msg[] = {"Disco says hello!\r\n"};
+/// data exchange
+#define INCOMING_PAYLOAD_SIZE (uint8_t)2
+#define CMD_TURN_BLUE_LED_ON (uint16_t)0x5841
+#define CMD_TURN_BLUE_LED_OFF (uint16_t)0x4659
+static char msg[] = {"Beep!\r\n"};
 
-#define TASKS_FREQUENCY_IN_MS 1000U
-#define TASKS_FREQUENCY_THRESHOLD (TASKS_FREQUENCY_IN_MS-1)
-
-int main(void)
-{
+int main(void) {
     /// enable PLL, and clock for an LED
     clocks_initialise();
 
-    /// configure uart
+    /// configure comms driver
     HAL_GPIO_Init(GPIOB, &uart1_tx_gpio);
     HAL_GPIO_Init(GPIOB, &uart1_rx_gpio);
-    comms_config_t comms_config = {
-        .payload_size = 10
+    comms_driver_config_t comms_driver_config = {
+        .payload_size = INCOMING_PAYLOAD_SIZE
     };
-    comms_initialise(comms_config);
+    comms_driver_initialise(comms_driver_config);
 
     /// initialise relevant GPIOs
     HAL_GPIO_Init(GPIOD, &led_gpio);
     HAL_GPIO_Init(GPIOC, &measure_gpio);
+    HAL_GPIO_Init(GPIOD, &led_blue_gpio);
 
     while(1) {
-        if ((time_counter == TASKS_FREQUENCY_THRESHOLD) & !background_processed) {
-            /// run all background tasks
+        if ((time_counter % TASKS_FREQUENCY_IN_MS == 0)
+        & !background_processed) {
+            /// run all background tasks at TASKS_FREQUENCY_IN_MS frequency
             HAL_GPIO_TogglePin(GPIOD, led_gpio.Pin);
-            comms_send_data((uint8_t*)&msg, sizeof(msg));
+            comms_driver_send_data((uint8_t*)&msg, sizeof(msg));
             background_processed = true;
         }
     }
@@ -74,7 +86,7 @@ int main(void)
 void SysTick_Handler(void) {
     /// toggle C1 to measure SysTick frequency on the scope
     HAL_GPIO_TogglePin(GPIOC, measure_gpio.Pin);
-    if (++time_counter == 1000) {
+    if (time_counter++ == TASKS_FREQUENCY_IN_MS) {
         time_counter = 0;
         background_processed = false;
     }
@@ -84,11 +96,25 @@ void HardFault_Handler(void) {
     clocks_system_reset();
 }
 
-void comms_error_cb(comms_error_t error) {
+void comms_driver_error_cb(comms_driver_error_t error) {
     (void)error;
 }
 
-void comms_handle_data_cb(uint8_t* payload, uint8_t payload_size) {
-    (void)payload;
-    (void)payload_size;
+void comms_driver_handle_data_cb(uint8_t* payload, uint8_t payload_size) {
+    static uint16_t cmd;
+    if (payload_size == INCOMING_PAYLOAD_SIZE) {
+        cmd = ((payload[0] << 8) | payload[1]);
+        switch (cmd) {
+            case CMD_TURN_BLUE_LED_ON: {
+                /// correspond to "XA"
+                HAL_GPIO_TogglePin(GPIOD, led_blue_gpio.Pin);
+                break;
+            }
+            case CMD_TURN_BLUE_LED_OFF: {
+                /// correspond to "FY"
+                HAL_GPIO_TogglePin(GPIOD, led_blue_gpio.Pin);
+                break;
+            }
+        }
+    }
 }
